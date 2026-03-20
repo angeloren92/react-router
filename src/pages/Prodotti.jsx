52cff00937acccc472d88f00fe9ba73ce5e8fb25
angeloren92 <angeloren92@gmail.com>
2026-03-20 14:31:46 +0100

    adj cards styling

--- src/pages/Prodotti.jsx
+++ src/pages/Prodotti.jsx
@@ -17,21 +17,35 @@ function Prodotti() {
   console.log(products)
 
   return (
-    <main>
+    <main className=" bg-dark  min-vh-100">
       <div className="container">
-        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-5">
+        <section className="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3">
           {
             products.map(({ id, title, category, description, image, price, rating }) => (
-          <div className="col g-4" key={id}>
-            <div className="card h-100">
-              <h2 className="h3">{title}</h2>
-              <img src={image} alt="" />
-            </div>
-          </div>
-          ))
+              <div className="col g-4" key={id}>
+                <div className="card bg-secondary-subtle h-100">
+                  <figure className="card-body d-flex flex-column justify-content-between align-items-center">
+                    <div className="position-relative">
+                      <img src={image} alt="{title}" className="img-fluid border bg-light rounded-3 p-5" />
+                      <figcaption className="position-absolute translate-middle fw-bold text-muted">{rating.rate} / {rating.count}</figcaption>
+                    </div>
+                    <h2 className="h5 text-start w-100 h-100 my-3">{title}</h2>
+                    <div className="d-flex justify-content-between w-100">
+                      <span className="text-muted">
+                        {category.toUpperCase()}
+                      </span>
+                      <span className="px-3 fs-5">
+                        € {price.toFixed(2)}
+                      </span>
+                    </div>
+
+                  </figure>
+                </div>
+              </div>
+            ))
           }
 
-        </div>
+        </section>
       </div>
     </main>
   )
