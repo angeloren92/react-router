2215fbf34cd8f45bd11b778e484474419ae6c83c
angeloren92 <angeloren92@gmail.com>
2026-03-20 12:49:44 +0100

    add fetch ApiCall

--- src/pages/Prodotti.jsx
+++ src/pages/Prodotti.jsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from "react"
+
+const apiUrl = 'https://fakestoreapi.com'
+
 function Prodotti() {
 
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    fetch(`${apiUrl}/products`)
+      .then(response => response.json())
+      .then(data => {
+        setProducts([data])
+      })
+  }, [])
+
+  console.log(products)
+
   return (
-    <>
-      <h1>PRODOTTI</h1>
-    </>
+    <main>
+      <div className="container">
+        <div className="row">
+
+
+        </div>
+      </div>
+    </main>
   )
 }
 
